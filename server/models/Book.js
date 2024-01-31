5e0ffad7ed48596223da875298af88fd4cd2d80e
christiecamp <christiecamphoto@gmail.com>
2024-01-30 19:03:38 -0500

    working through models - created book subdoc schema

---
+++ server/models/Book.js
@@ -0,0 +1,26 @@
+const { Schema } = require('mongoose');
+
+//subdocument schema for books
+const bookSchema = new Schema({
+    authors: [{ type: String }],
+    description: {
+        type: String,
+        required: true,
+    },
+    bookId: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+    },
+    link: {
+        type: String,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+});
+
+module.exports = bookSchema;
