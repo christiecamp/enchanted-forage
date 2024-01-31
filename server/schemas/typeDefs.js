83c4778c0ccfd5282a5f302093e7f49ab068c276
christiecamp <christiecamphoto@gmail.com>
2024-01-30 19:39:27 -0500

    finalized typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,11 +1,44 @@
 const { gql } = require('apollo-server-express');
 const { typeDefs } = require('.');
 
-//bookinput
-//user
-//book
-//auth
-//query
-//mutation
+//typeDefs is a string that defines the shape of the data & specifies what queries can be used to fetch the data
+//mutations are used to modify data
+const typeDefs = gql`
+    input BookInput {
+        bookId: ID!
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+    type User {
+        id: ID
+        username: String
+        email: String
+        savedBooks: [Book]
+    }
+    type Book {
+        bookId: ID!
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
+    type Query {
+        me: User
+    }
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(input: BookInput): User
+        removeBook(bookId: ID!): User
+    }
+`;
 
 module.exports = typeDefs;
