4391c1470ad2059352e9282a73fd30c9cd5237a9
christiecamp <christiecamphoto@gmail.com>
2024-01-31 09:49:19 -0500

    set up skeletal files for query & mutations while working through saved page

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,14 @@
+import { gql } from '@apollo/client';
+
+//login mutation
+export const LOGIN_USER = gql`
+
+`;
+
+//create user mutation
+
+//save book mutation
+
+//remove book mutation
+
+//search google books api mutation
