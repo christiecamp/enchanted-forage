92c6a8d8c54caf8e99bfe5c50ae91c499f0fa3b4
christiecamp <christiecamphoto@gmail.com>
2024-01-30 22:32:49 -0500

    updates to client folder and working through App

---
+++ client/src/App.jsx
@@ -0,0 +1,6 @@
+import './App.css';
+import { Outlet } from 'react-router-dom';
+
+import SearchBooks from './pages/SearchBooks';
+import SavedBooks from './pages/SavedBooks';
+import Navbar from './components/Navbar';
