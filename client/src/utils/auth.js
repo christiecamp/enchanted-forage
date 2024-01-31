60a63da55832df6fdb41275face4a06455813f1b
christiecamp <christiecamphoto@gmail.com>
2024-01-31 12:11:52 -0500

    created client side auth

---
+++ client/src/utils/auth.js
@@ -0,0 +1,50 @@
+//decode token & get user info
+import decode from 'jwt-decode';
+
+//instantiate a new AuthService object
+class AuthService {
+    //get user data
+    getProfile() {
+        return decode(this.getToken());
+    }
+
+    //check if user's logged in
+    loggedIn() {
+        //check for saved token & it's still valid
+        const token = this.getToken();
+        return !!token && !this.isTokenExpired(token);
+    }
+
+    //check if token is expired
+    isTokenExpired(token) {
+        try {
+            const decoded = decode(token);
+            if (decoded.exp < Date.now() / 1000) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (err) {
+            return false;
+        }
+    }
+
+    //retrieve token from localStorage
+    getToken() {
+        return localStorage.getItem('id_token');
+    }
+
+    //set token to localStorage & reload page to homepage
+    login(idToken) {
+        localStorage.setItem('id_token', idToken);
+        window.location.assign('/');
+    }
+
+    //clear token from localStorage & force logout with reload
+    logout() {
+        localStorage.removeItem('id_token');
+        window.location.assign();
+    }
+}
+
+export default new AuthService();
