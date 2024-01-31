687d88639a44e7678303786c27511772abc0a5f7
christiecamp <christiecamphoto@gmail.com>
2024-01-30 21:44:07 -0500

    working through user controller

---
+++ server/controllers/user-controller.js
@@ -0,0 +1,13 @@
+//import user model
+const { User } = require('../models');
+//import auth
+const { signToken } = require('../utils/auth');
+
+module.exports = {
+    //get single user - id or username
+    //create user & token - send back to client
+    //login - check credentials & send back token to client
+    //save book - add to user's `savedBooks` field
+    //remove book - pull & remove from user's `savedBooks` field 
+
+};
