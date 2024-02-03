ec148ec61fdf046302a8698463a1c405701c76f1
christiecamp <christiecamphoto@gmail.com>
2024-02-03 18:06:45 -0500

    making updates to deploy on heroku

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    base: "./", // Add this line
+    outDir: "dist", // Output directory for the build.
+    emptyOutDir: true, // Empty the output directory when the build starts.
+},
   server: {
     port: 3000,
     open: true,
--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-require ('dotenv').config();
 
 //connect to mongoDB database
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/enchantedforage');
